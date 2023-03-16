bca2dddc88901a2ac1d407bfa1282c099340a16c
Emmanuel Scopelliti <escopelliti@unibz.it>
2023-03-16 19:53:36 +0100

    ES: Updated CalendarEventController from class to Interface

--- src/main/java/it/unibz/taskcalendarservice/CalendarEventRepository.java
+++ src/main/java/it/unibz/taskcalendarservice/CalendarEventRepository.java
@@ -1,5 +1,5 @@
 package it.unibz.taskcalendarservice;
 
-public class CalendarEventRepository {
+public interface CalendarEventRepository {
 //    This interface should provide methods for persisting and retrieving CalendarEvent entities to/from a data store
 }
