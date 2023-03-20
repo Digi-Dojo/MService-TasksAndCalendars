acca9b9ec87e5fef97c3f9dccf5e5685a782f0a1
AntonioCoppe <46423374+AntonioCoppe@users.noreply.github.com>
2023-03-20 23:12:10 +0100

    fixed merge conflicts

--- src/main/java/it/unibz/taskcalendarservice/Place.java
+++ src/main/java/it/unibz/taskcalendarservice/Place.java
@@ -1,22 +1,4 @@
 package it.unibz.taskcalendarservice;
 
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-public class Place {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String address;
-
-    @OneToMany(mappedBy = "place")
-    private List<Task> tasks;
-
-    @OneToMany(mappedBy = "place")
-    private List<CalendarEvent> calendarEvents;
-
-    // Getters and setters
+public record Place(Long id, String name) {
 }
--- src/main/java/it/unibz/taskcalendarservice/User.java
+++ src/main/java/it/unibz/taskcalendarservice/User.java
@@ -1,18 +1,4 @@
 package it.unibz.taskcalendarservice;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-
-    // Getters and setters
+public record User(Long id, String name) {
 }
