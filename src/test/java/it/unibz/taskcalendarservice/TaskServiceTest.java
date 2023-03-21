09015f8d5afb554dbe7c9c475417c0c45809c201
AndrejicDanica <dandrejic@unibz.it>
2023-03-21 11:00:21 +0100

    Created tests in DatabaseConfigTest and TaskServiceTest

--- src/test/java/it/unibz/taskcalendarservice/TaskServiceTest.java
+++ src/test/java/it/unibz/taskcalendarservice/TaskServiceTest.java
@@ -1,4 +1,103 @@
 package it.unibz.taskcalendarservice;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskServiceTest {
+    @Autowired
+    private TaskRepository taskRepo;
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    void createTaskTest(){
+        String description = "Test task";
+        Task.Status status = Task.Status.DONE;
+
+        Task createdTask = taskService.createTask(description, status);
+
+        Optional<Task> retrievedTask = taskRepo.findById(createdTask.getId());
+        assertTrue(retrievedTask.isPresent());
+        assertEquals(description, retrievedTask.get().getDescription());
+        assertEquals(status, retrievedTask.get().getStatus());
+    }
+
+    @Test
+    void updateTaskDescriptionTest(){
+
+        // create a new task and save it to the repository
+        Task task = new Task("Task 1", null);
+        task = taskRepo.save(task);
+
+        boolean updated = taskService.updateTaskDescription(task.getId(), "New description");
+
+        assertTrue(updated);
+        Optional<Task> updatedTask = taskRepo.findById(task.getId());
+        assertTrue(updatedTask.isPresent());
+        assertEquals("New description", updatedTask.get().getDescription());
+    }
+
+    @Test
+    void updateTaskStatusTest(){
+        Task task = new Task("Test task", Task.Status.PENDING);
+        taskRepo.save(task);
+
+        boolean result = taskService.updateTaskStatus(task.getId(), Task.Status.DONE);
+        assertTrue(result);
+
+        Optional<Task> updatedTask = taskRepo.findById(task.getId());
+        assertTrue(updatedTask.isPresent());
+        assertEquals(Task.Status.DONE, updatedTask.get().getStatus());
+    }
+
+    @Test
+    void updateTaskUserTest(){
+
+        Task task = new Task("", null);
+        taskRepo.save(task);
+
+        User user = new User(0L,"John");
+
+        boolean result = taskService.updateTaskUser(task.getId(),user);
+        assertTrue(result);
+
+        assertNotNull(taskService.getTaskById(task.getId()).getUser());
+
+    }
+
+    @Test
+    void deleteTaskTest() {
+        Task task = new Task("Test task", Task.Status.PENDING);
+        taskRepo.save(task);
+
+        boolean result = taskService.deleteTask(task.getId());
+        assertTrue(result);
+
+        Optional<Task> deletedTask = taskRepo.findById(task.getId());
+        assertTrue(deletedTask.isEmpty());
+    }
+
+    @Test
+    void testUpdateTaskTags() {
+        // create a task with tags to update
+        Task task = new Task("Test Task", Task.Status.PENDING);
+        task.setTags(Arrays.asList("tag1", "tag2"));
+        taskRepo.save(task);
+
+        List<String> newTags = Arrays.asList("tag2", "tag3", "tag4");
+
+        boolean result = taskService.updateTaskTags(task.getId(), newTags);
+        assertTrue(result);
+
+        Optional<Task> updatedTask = taskRepo.findById(task.getId());
+        assertTrue(updatedTask.isPresent());
+        assertEquals(newTags, updatedTask.get().getTags());
+    }
 }
--- src/test/java/it/unibz/taskcalendarservice/config/DatabaseConfigTest.java
+++ src/test/java/it/unibz/taskcalendarservice/config/DatabaseConfigTest.java
@@ -1,5 +1,62 @@
 package it.unibz.taskcalendarservice.config;
 
+import it.unibz.taskcalendarservice.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DatabaseConfigTest {
 
+    @Test
+    void createCalendarEventTest() throws SQLException {
+
+        CalendarEventController cec0 = new CalendarEventController("",null, null, null,null, null);
+        assertNull(cec0,"Method should return null");
+
+        User user0 = new User(1L,"John");
+        Place place0 = new Place(1L,"Something");
+
+        LocalDate date1 = LocalDate.of(2023, 3, 20);
+        LocalTime time1 = LocalTime.of(22,0,0);
+        LocalDateTime startDate = LocalDateTime.of(date1,time1);
+
+        LocalDate date2 =  LocalDate.of(2023, 3, 21);
+        LocalTime time2 = LocalTime.of(24,0,0);
+        LocalDateTime endDate = LocalDateTime.of(date2,time2);
+
+        String[] list = {"Something, Otherthing"};
+        List<String> tags = Arrays.asList(list);
+
+        CalendarEventController cec1 = new CalendarEventController("First Calendar", startDate, endDate, user0,place0, tags );
+        assertAll((Executable) cec1);
+
+        assertNotNull(cec1.getCalendarUser());
+        assertNotNull(cec1.getCalendarEvent());
+        assertNotNull(cec1.getCalendarPlace());
+
+        cec1.createCalendarEvent();
+        /*Task task1 = new Task("New task", null);
+        task1.setUser(user0);
+        task1.setTags(tags);
+        assertNotNull(task1.getId());
+        assertTrue(task1.getDescription().length()>0);
+
+        cec1.updateCalendarEvent(task1.getId().intValue());
+
+        assertNotNull(cec1.getCalendarTags(task1.getId().intValue()));
+        assertNotNull(cec1.getCalendarDescription(task1.getId().intValue()));
+
+        assertEquals(task1.getDescription(),"New task");*/
+
+
+
+    }
 }
