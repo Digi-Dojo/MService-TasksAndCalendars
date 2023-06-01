e348bd28e83ff221a84626bbe48fe7726476943c
acoppe <acoppe@unibz.it>
2023-06-01 19:22:21 +0200

    fixed exception erros for task and calendar

--- src/main/java/it/unibz/taskcalendarservice/task/domain/SearchTask.java
+++ src/main/java/it/unibz/taskcalendarservice/task/domain/SearchTask.java
@@ -24,8 +24,6 @@ public class SearchTask {
     public List<Task> getAll(){
         List<Task> tasks = taskRepository.findAll();
         System.out.println("\nTaskList size: " + tasks.size());
-
-        if (tasks.size() == 0) throw new IllegalArgumentException("No tasks were found");
         return tasks;
     }
 }
